14169c12ec4e3c9c4af59c590aab21ebeb551db1
hieuculi34 <hieuculi34@gmail.com>
2020-04-21 16:20:05 +0700

    Hieu up

--- QL_GV_HS_THPT/QL_GV_HS_THPT/GUI/frmTKB.Designer.cs
+++ QL_GV_HS_THPT/QL_GV_HS_THPT/GUI/frmTKB.Designer.cs
@@ -32,7 +32,17 @@
             this.cbbmalop = new System.Windows.Forms.ComboBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.dgvtkb = new System.Windows.Forms.DataGridView();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnthem = new System.Windows.Forms.Button();
+            this.btnsua = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnxoa = new System.Windows.Forms.Button();
+            this.txtthu = new System.Windows.Forms.TextBox();
+            this.txttiet = new System.Windows.Forms.TextBox();
+            this.cbbbgiaovien = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtmon = new System.Windows.Forms.TextBox();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvtkb)).BeginInit();
             this.SuspendLayout();
@@ -40,20 +50,21 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(12, 34);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(82, 15);
+            this.label1.Size = new System.Drawing.Size(69, 15);
             this.label1.TabIndex = 0;
             this.label1.Text = "Tên lớp học";
             // 
             // cbbmalop
             // 
             this.cbbmalop.FormattingEnabled = true;
-            this.cbbmalop.Location = new System.Drawing.Point(130, 33);
+            this.cbbmalop.Location = new System.Drawing.Point(100, 34);
             this.cbbmalop.Name = "cbbmalop";
-            this.cbbmalop.Size = new System.Drawing.Size(103, 21);
+            this.cbbmalop.Size = new System.Drawing.Size(70, 21);
             this.cbbmalop.TabIndex = 1;
+            this.cbbmalop.SelectedIndexChanged += new System.EventHandler(this.Cbbmalop_SelectedIndexChanged);
             // 
             // groupBox1
             // 
@@ -61,9 +72,9 @@
             this.groupBox1.Controls.Add(this.dgvtkb);
             this.groupBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox1.Location = new System.Drawing.Point(0, 95);
+            this.groupBox1.Location = new System.Drawing.Point(0, 253);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(551, 241);
+            this.groupBox1.Size = new System.Drawing.Size(534, 165);
             this.groupBox1.TabIndex = 2;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Thời khóa biểu";
@@ -74,28 +85,133 @@
             this.dgvtkb.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgvtkb.Location = new System.Drawing.Point(3, 17);
             this.dgvtkb.Name = "dgvtkb";
-            this.dgvtkb.Size = new System.Drawing.Size(548, 221);
+            this.dgvtkb.Size = new System.Drawing.Size(525, 180);
             this.dgvtkb.TabIndex = 0;
+            this.dgvtkb.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.Dgvtkb_CellClick);
             // 
-            // button1
+            // btnthem
             // 
-            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
-            this.button1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(269, 18);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 58);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "Show";
-            this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            this.btnthem.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btnthem.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnthem.Location = new System.Drawing.Point(335, 20);
+            this.btnthem.Name = "btnthem";
+            this.btnthem.Size = new System.Drawing.Size(93, 34);
+            this.btnthem.TabIndex = 3;
+            this.btnthem.Text = "Thêm";
+            this.btnthem.UseVisualStyleBackColor = false;
+            this.btnthem.Click += new System.EventHandler(this.Btnthem_Click);
+            // 
+            // btnsua
+            // 
+            this.btnsua.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btnsua.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnsua.Location = new System.Drawing.Point(335, 75);
+            this.btnsua.Name = "btnsua";
+            this.btnsua.Size = new System.Drawing.Size(93, 36);
+            this.btnsua.TabIndex = 4;
+            this.btnsua.Text = "Sửa";
+            this.btnsua.UseVisualStyleBackColor = false;
+            this.btnsua.Click += new System.EventHandler(this.Btnsua_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(27, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Thứ :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(27, 118);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Tiết :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(27, 201);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 15);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Giáo viên :";
+            this.label4.Click += new System.EventHandler(this.Label4_Click);
+            // 
+            // btnxoa
+            // 
+            this.btnxoa.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btnxoa.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnxoa.Location = new System.Drawing.Point(335, 134);
+            this.btnxoa.Name = "btnxoa";
+            this.btnxoa.Size = new System.Drawing.Size(93, 34);
+            this.btnxoa.TabIndex = 8;
+            this.btnxoa.Text = "Xóa";
+            this.btnxoa.UseVisualStyleBackColor = false;
+            // 
+            // txtthu
+            // 
+            this.txtthu.Location = new System.Drawing.Point(100, 75);
+            this.txtthu.Name = "txtthu";
+            this.txtthu.Size = new System.Drawing.Size(143, 20);
+            this.txtthu.TabIndex = 9;
+            // 
+            // txttiet
+            // 
+            this.txttiet.Location = new System.Drawing.Point(100, 116);
+            this.txttiet.Name = "txttiet";
+            this.txttiet.Size = new System.Drawing.Size(143, 20);
+            this.txttiet.TabIndex = 11;
+            // 
+            // cbbbgiaovien
+            // 
+            this.cbbbgiaovien.FormattingEnabled = true;
+            this.cbbbgiaovien.Location = new System.Drawing.Point(100, 199);
+            this.cbbbgiaovien.Name = "cbbbgiaovien";
+            this.cbbbgiaovien.Size = new System.Drawing.Size(143, 21);
+            this.cbbbgiaovien.TabIndex = 12;
+            this.cbbbgiaovien.SelectedIndexChanged += new System.EventHandler(this.Cbbbgiaovien_SelectedIndexChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(27, 157);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(60, 15);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Môn học :";
+            // 
+            // txtmon
+            // 
+            this.txtmon.Location = new System.Drawing.Point(100, 157);
+            this.txtmon.Name = "txtmon";
+            this.txtmon.Size = new System.Drawing.Size(143, 20);
+            this.txtmon.TabIndex = 14;
             // 
             // frmTKB
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.ClientSize = new System.Drawing.Size(551, 336);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(534, 418);
+            this.Controls.Add(this.txtmon);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.cbbbgiaovien);
+            this.Controls.Add(this.txttiet);
+            this.Controls.Add(this.txtthu);
+            this.Controls.Add(this.btnxoa);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnsua);
+            this.Controls.Add(this.btnthem);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.cbbmalop);
             this.Controls.Add(this.label1);
@@ -116,6 +232,16 @@
         private System.Windows.Forms.ComboBox cbbmalop;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.DataGridView dgvtkb;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnthem;
+        private System.Windows.Forms.Button btnsua;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnxoa;
+        private System.Windows.Forms.TextBox txtthu;
+        private System.Windows.Forms.TextBox txttiet;
+        private System.Windows.Forms.ComboBox cbbbgiaovien;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtmon;
     }
 }
--- QL_GV_HS_THPT/QL_GV_HS_THPT/GUI/frmTKB.cs
+++ QL_GV_HS_THPT/QL_GV_HS_THPT/GUI/frmTKB.cs
@@ -20,23 +20,129 @@ namespace QL_GV_HS_THPT.GUI
         private void FrmTKB_Load(object sender, EventArgs e)
         {
             db.loadComboBox(cbbmalop, "select MaLopHoc from LopHoc ");
+            db.loadComboBox(cbbbgiaovien, "SELECT ID_GV FROM dbo.GiaoVien ");
         }
 
-        private void Button1_Click(object sender, EventArgs e)
+        private void Cbbmalop_SelectedIndexChanged(object sender, EventArgs e)
         {
-            if (cbbmalop.SelectedItem == null)
+            string tkb = "EXEC TKB @malop='" + cbbmalop.Text + "'";
+            db.loadDataGridView(dgvtkb, tkb);
+
+        }
+
+        private void Btnthem_Click(object sender, EventArgs e)
+        {
+            if (btnthem.Text == "Thêm")
             {
-                MessageBox.Show("Hãy chọn mã lớp!");
+                cbbmalop.Text = "";
+                txtthu.Clear();
+                txttiet.Clear();
+                cbbbgiaovien.Text = "";
+                btnsua.Enabled = false;
+                btnxoa.Enabled = false;
+                btnthem.Text = "Lưu";
             }
             else
             {
-                string tkb = "EXEC TKB @malop='" + cbbmalop.Text + "'";
-                db.loadDataGridView(dgvtkb, tkb);
-                dgvtkb.Columns[0].Width = 115;
-                dgvtkb.Columns[1].Width = 115;
-                dgvtkb.Columns[2].Width = 115;
-                dgvtkb.Columns[3].Width = 150;
+                btnthem.Text = "Thêm";
+                btnsua.Enabled = true;
+                btnxoa.Enabled = true;
+                try
+                {
+                    string malop = cbbmalop.Text.Trim();
+                    string magv = cbbbgiaovien.Text.Trim();
+                    string thu = txtthu.Text.Trim();
+                    string tiet = txttiet.Text.Trim();
+                    string mon = txtmon.Text.Trim();
+
+                    if (cbbmalop.Text.Length != 0 && cbbbgiaovien.Text.Length != 0 && txtthu.Text.Length != 0 && txttiet.Text.Length != 0)
+                    {
+                        bool check = db.Check(cbbmalop.Text, "SELECT MaLopHoc FROM dbo.DayHoc WHERE Thu = N'" + thu + "' AND Tiet =N'" + tiet + "'");
+                        if (check == false)
+                        {
+                            db.ThucThiKetNoi("INSERT dbo.DayHoc(MonHoc, ID_GV, MaLopHoc, Thu, Tiet) VALUES (N'" + mon + "', '" + magv + "', '" + malop + "', N'" + thu + "', N'" + tiet + "')");
+                            MessageBox.Show("Thêm hoàn tất!");
+                            db.loadDataGridView(dgvtkb, "EXEC TKB @malop = '" + malop + "'");
+                        }
+                        else
+                        {
+                            MessageBox.Show("Không thể thêm!" + txtthu.Text + " đã có tiết " + txttiet + ".", "", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Hãy điền đủ thông tin!", "", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    }
+
+                }
+                catch
+                {
+                    MessageBox.Show("Đã xảy ra lỗi!", "", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+            
+        }
+
+        private void Cbbbgiaovien_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Dgvtkb_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+            int i;
+            i = dgvtkb.CurrentRow.Index;
+            txtthu.Text = dgvtkb.Rows[i].Cells[0].Value.ToString();
+            txttiet.Text = dgvtkb.Rows[i].Cells[1].Value.ToString();
+            txtmon.Text = dgvtkb.Rows[i].Cells[2].Value.ToString();
+            cbbbgiaovien.Text = dgvtkb.Rows[i].Cells[3].Value.ToString();
+        }
+
+        private void Btnsua_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string malop = cbbmalop.Text.Trim();
+                string magv = cbbbgiaovien.Text.Trim();
+                string thu = txtthu.Text.Trim();
+                string tiet = txttiet.Text.Trim();
+                string mon = txtmon.Text.Trim();
+
+                if (cbbmalop.Text.Length != 0 && cbbbgiaovien.Text.Length != 0 && txtthu.Text.Length != 0 && txttiet.Text.Length != 0)
+                {
+                    bool check = db.Check(malop, "SELECT MaLopHoc FROM dbo.DayHoc");
+                    if (check == true)
+                    {
+                        
+                        string update = "UPDATE dbo.DayHoc SET MonHoc = N'" + mon + "', ID_GV = N'" + magv + "', Thu=N'" + thu + "', Tiet=N'" + tiet + "' WHERE MaLopHoc=N'" + malop + "'";
+                        db.ThucThiKetNoi(update);
+                        MessageBox.Show("Sửa thông tin cho TKB hoàn tất!");
+                        dgvtkb.DataSource = null;
+                        db.loadDataGridView(dgvtkb, "EXEC TKB @malop = '" + malop + "'");
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("Lớp học không tồn tại!", "", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Bạn chưa điền đầy đủ thông tin!", "", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Đã xảy ra lỗi!", "", MessageBoxButtons.OK, MessageBoxIcon.Warning);
             }
         }
+
     }
 }
+
